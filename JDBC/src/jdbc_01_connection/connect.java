8ac01763d4f429545212811cc7dc9724c4a663da
Shambhavisharma13 <shambhavisharma8235@gmail.com>
2026-02-19 13:05:36 +0530

    Add files via upload

---
+++ JDBC/src/jdbc_01_connection/connect.java
@@ -0,0 +1,31 @@
+package jdbc_01_connection;
+
+import java.sql.Connection;     // missing import
+import java.sql.DriverManager;
+
+public class connect {
+    public static void main(String[] args) {
+        try {
+            // Load driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Establish connection
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/AdvanceJava",
+                "root",
+                "8235618645"
+            );
+
+            if (con != null) {
+                System.out.println("Connection Established");
+            } else {
+                System.out.println("Connection Not Established");
+            }
+
+            con.close();  // good practice
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
