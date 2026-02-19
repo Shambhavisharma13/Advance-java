f09a257d794b1e6676d3332734a6ed516b281612
Shambhavisharma13 <shambhavisharma8235@gmail.com>
2026-02-19 13:12:04 +0530

    Add files via upload

---
+++ JDBC/src/jdbc_01_connection/connect_opt.java
@@ -0,0 +1,30 @@
+package jdbc_01_connection;
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+
+public class connect_opt {
+	public static final String loadDriver="com.mysql.cj.jdbc.Driver";
+	public static final String url= "jdbc:mysql://localhost:3306/AdvanceJava";
+	public static final String username= "root";
+	public static final String password="12345";
+	
+	
+	 public static void main(String[] args) {
+		 try {
+			 Class.forName(loadDriver);
+			 
+			Connection con=DriverManager.getConnection(url,username,password);
+			if(con!=null) {
+				System.out.println("connection done");
+			}
+			else {
+				System.out.println("connection not done");
+			}
+		 }catch(Exception e) {
+			 e.printStackTrace();
+		 }
+		
+	}
+
+}
