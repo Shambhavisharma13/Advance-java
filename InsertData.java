fa5629d529faf1fda44e46ad18849e8dccb4dd93
Shambhavisharma13 <shambhavisharma8235@gmail.com>
2026-02-20 09:12:09 +0530

    Add files via upload
    insertion of data in database

---
+++ InsertData.java
@@ -0,0 +1,36 @@
+package jdbc_02_statements;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class InsertData {
+	public static final String loadDriver="com.mysql.cj.jdbc.Driver";
+	public static final String url= "jdbc:mysql://localhost:3306/AdvanceJava";
+	public static final String username= "root";
+	public static final String password="8235618645";
+	
+	public static final String QUERY="insert into stud2 values(101,'Aman',20),(102,'Lakshya',22),(103,'Harsh',21)";
+	public static void main(String[] args) {
+		
+		 try {
+			 Class.forName(loadDriver);
+			 
+			Connection con=DriverManager.getConnection(url,username,password);
+			
+			Statement st=con.createStatement();
+			
+			int k=st.executeUpdate(QUERY);
+			if(k<0) {
+				System.out.println("data inserted");
+			}else {
+				System.out.println("Data not inserted");
+			}
+		 }
+		 catch(Exception e) {
+			 e.printStackTrace();
+		 }
+		
+	}
+
+}
